9c99162ba6e2c4aca120b1fbea046291281f7dc2
Theo <theo.richard8654@gmail.com>
2026-03-01 05:26:09 +0100

    Improve readability of variant proxy ID validation by formatting the regex condition

--- src/server/services/twitch.service.ts
+++ src/server/services/twitch.service.ts
@@ -576,7 +576,9 @@ function registerVariantProxyTarget(targetUrl: string): string {
 
 function resolveVariantProxyTarget(proxyTargetId: string): string {
   const normalizedId = (proxyTargetId || '').trim();
-  if (!/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(normalizedId)) {
+  if (
+    !/^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(normalizedId)
+  ) {
     throw new Error('Invalid variant proxy id');
   }
 
